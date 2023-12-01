57a316d22a84175896bbdc310e30684600af5875
Jonas Westerlund <login@jonaswesterlund.fi>
2023-12-01 14:16:27 +0200

    Day 1 done

--- 1/main.go
+++ 1/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hi")
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	reader := bufio.NewScanner(file)
+	sum := 0
+	for reader.Scan() {
+		line := reader.Text()
+		numbers := make([]int, 0)
+		for _, char := range line {
+			if num, err := strconv.Atoi(string(char)); err == nil {
+				numbers = append(numbers, num)
+
+			}
+		}
+		value, _ := strconv.Atoi(strconv.Itoa(numbers[0]) + strconv.Itoa(numbers[len(numbers)-1]))
+		sum = sum + value
+	}
+	fmt.Print(sum)
 }
